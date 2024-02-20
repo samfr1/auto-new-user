4ba98184500d4225a7222ec4ab5ab55d75b8e8fd
Sam <97795658+samfr1@users.noreply.github.com>
2024-02-20 16:27:12 +0600

    Update new-user.sh

--- new-user.sh
+++ new-user.sh
@@ -1,3 +1,4 @@
+```bash
 #!/bin/bash
 
 # Create a new user with superuser privileges
@@ -8,18 +9,19 @@ sudo useradd -m -s /bin/bash userroot
 # Установка пароля для нового пользователя
 echo "userroot:passwordroot" | sudo chpasswd
 
-# Retrieve the external IP address
-# Получение внешнего IP-адреса
+# Retrieve the external IPv4 address
+# Получение внешнего IPv4-адреса
 external_ip=$(curl -s ifconfig.me)
 
 # Port for connection (you can change it to your desired port)
 # Порт для подключения (можно изменить на нужный вам порт)
 port=22
 
-# Print out the connection details
-# Вывод данных для подключения
-echo "Для подключения используйте следующие данные:"
-echo "IP-адрес: $external_ip"
-echo "Логин: userroot"
-echo "Пароль: passwordroot"
-echo "Порт: $port"
+# Print out the connection details in English
+# Вывод данных для подключения на английском
+echo "Use the following details for connection:"
+echo "IP Address: $external_ip"
+echo "Username: userroot"
+echo "Password: passwordroot"
+echo "Port: $port"
+```
