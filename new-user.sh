2e716d9e71ea5c0adf6eabb73bf8c18011943925
Sam <97795658+samfr1@users.noreply.github.com>
2024-02-20 16:39:10 +0600

    Update new-user.sh

--- new-user.sh
+++ new-user.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-# Create a new user with superuser privileges
+# Проверка наличия прав root
+if [ "$(id -u)" -ne "0" ]; then
+    echo "Этот скрипт должен быть запущен от имени пользователя с правами root." 1>&2
+    exit 1
+fi
+
 # Создание нового пользователя с суперправами
-sudo useradd -m -s /bin/bash userroot
+useradd -m -s /bin/bash userroot
 
-# Set the password for the new user
 # Установка пароля для нового пользователя
-echo "userroot:passwordroot" | sudo chpasswd
+echo "userroot:passwordroot" | chpasswd
 
-# Retrieve the external IP address
 # Получение внешнего IP-адреса
 external_ip=$(curl -s ifconfig.me)
 
-# Port for connection (you can change it to your desired port)
 # Порт для подключения (можно изменить на нужный вам порт)
 port=22
 
-# Print out the connection details
 # Вывод данных для подключения
 echo "Use the following details for connection:"
 echo "IP Address: $external_ip"
